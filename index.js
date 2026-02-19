42e2788d5b16023cfd60e251d46d7866097df4d7
NPC1099 <eududu881@gmail.com>
2026-02-19 19:31:59 -0300

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,10 @@ const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 (async () => {
   try {
     await rest.put(
-      Routes.applicationCommands(process.env.CLIENT_ID),
+      Routes.applicationGuildCommands(
+  process.env.CLIENT_ID,
+  process.env.GUILD_ID
+)),
       { body: commands }
     );
     console.log("Slash command registrado.");
